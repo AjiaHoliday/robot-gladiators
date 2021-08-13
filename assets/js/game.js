0cb3ca3a815a86972d715134e1b80581ae160610
ajia <ajialeeholiday@gmail.com>
2021-08-12 19:22:28 -0600

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,36 +69,87 @@ var fight = function(enemyName) {
   }
 };
 
-
+//function to start a new game
+var startGame = function() {
+  // reset player state
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 // fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++){
-  // if player is still alive, keep fighting
-  if (playerHealth > 0)  {
-    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert("Welcome to Robot Gladiators!" + (i+1));
+  for (var i = 0; i < enemyNames.length; i++) {
+    // if player is still alive, keep fighting
+    if (playerHealth > 0)  {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiators! Round: " + (i+1));
 
-    // pick new enemy to fight based on the index of the enemyNames array
-    var pickedEnemyName = enemyNames[i];
+      // pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
 
-    // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
 
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
+      // use debugger to pause script from running and check what's going on at that moment in the code
+      // debugger;
 
-    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-    fight(pickedEnemyName);
-  }
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
+    }
 // if player isn't alive, stop the game
-  else {
-    window.alert('You have lost your robot in battle! Game Over!');
-    break;
+    else {
+      window.alert('You have lost your robot in battle! Game Over!');
+      break;
+    }
   }
-}
+  // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+  endGame();
+
+  // play again
+  startGame();
+};
+
+//debugger;
+
+ 
+// funtion to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth >0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney +".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
 
 
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
+// ask player if they would like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon")
+  }
+};
+// start the game when the page loads
+startGame();
+
+//Wrap the game logic in a startGame() function
+//When the player is defeated or there are no more enemies, call an endGame() function that:
+//    Alerts the player's total stats
+//    Asks the player if they want to play again
+//    If yes, call startGame() to restart the game
+//After the player skips or defeats an enemy (and there are still more robots to fight):
+//    Ask the player if they want to "shop"
+//    If no, continue as normal
+//    If yes, call the shop() function
+//    In the shop() function, ask player if they want to "refill" health, "upgrade" attack, or "leave" the shop
+//    If refill, subtract money points from player and increase health
+//    If upgrade, subtract money points from player and increase attack power
+//    If leave, alert goodbye and exit the function
+//    If any other invalid option, call shop() again
+//
+//
+//
+//
