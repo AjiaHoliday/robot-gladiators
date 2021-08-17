2ddc64cf3d9a0fc1ff1690e64bb664ad5febd1c9
ajia <ajialeeholiday@gmail.com>
2021-08-17 14:58:32 -0600

    fixed null name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,8 +169,18 @@ var shop = function() {
 /*GAME INFORMATION / VARIABLES */
 
 // Player information
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
